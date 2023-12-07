64692248b2a57d60a8d4ca82714adcd33dda04ce
Wizen <guzmanpjaime@gmail.com>
2023-12-07 09:40:29 -0600

    added dirty to document and editor to check if document is different from when it started to prompt user to save

--- src/document.rs
+++ src/document.rs
@@ -1,11 +1,13 @@
 use crate::Row;
 use crate::Position;
 use std::fs;
+use std::io::{Error, Write};
 
 #[derive(Default)]
 pub struct Document {
     rows: Vec<Row>,
     pub file_name: Option<String>,
+    dirty: bool,
 }
 
 impl Document {
@@ -18,6 +20,7 @@ impl Document {
         Ok(Self{
             rows,
             file_name: Some(filename.to_string()),
+            dirty: false,
         })
     }
     pub fn row(&self, index: usize) -> Option<&Row> {
@@ -30,9 +33,6 @@ impl Document {
         self.rows.len()
     }
     fn insert_rowline(&mut self, at: &Position) {
-        if at.y > self.len() {
-            return;
-        }
         if at.y == self.len() {
             self.rows.push(Row::default());
             return ;
@@ -41,6 +41,10 @@ impl Document {
         self.rows.insert(at.y + 1, new_row);
     }
     pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y > self.len() {
+            return;
+        }
+        self.dirty = true;
         if c == '\n' {
             self.insert_rowline(at);
             return;
@@ -49,7 +53,7 @@ impl Document {
             let mut row = Row::default();
             row.insert(0,c);
             self.rows.push(row);
-        } else if at.y < self.len() {
+        } else {
             let row = self.rows.get_mut(at.y).unwrap();
             row.insert(at.x, c)
         }
@@ -67,5 +71,20 @@ impl Document {
             let row = self.rows.get_mut(at.y).unwrap();
             row.delete(at.x);
         }
+        self.dirty = true;
+    }
+    pub fn save(&mut self) -> Result<(), Error> {
+        if let Some(file_name) = &self.file_name {
+            let mut file = fs::File::create(file_name)?;
+            for row in &self.rows {
+                file.write_all(row.as_bytes())?;
+                file.write_all(b"\n")?;
+            }
+        }
+        self.dirty = false;
+        Ok(())
+    }
+    pub fn is_dirty(&self) -> bool {
+        self.dirty
     }
 }
